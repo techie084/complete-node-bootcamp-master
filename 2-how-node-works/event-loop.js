6c609aa3331e0b0a61f8edb63c2714ac543702a8
Techie 🤸️🤍️ <wisdommatthew08@gmail.com>
2025-06-06 13:15:59 +0100

    fix: random changes

---
+++ 2-how-node-works/event-loop.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 4;
+// Error
+
+setTimeout(() => console.log("Timer 1 finished "), 0);
+setImmediate(() => console.log("immediate 1 finished"));
+
+fs.readFile("test-fle.txt", () => {
+  console.log("i/o finished");
+  console.log("-----------------");
+
+  setTimeout(() => console.log("Timer 2 finished "), 0);
+  setTimeout(() => console.log("Timer 3 finished "), 3000);
+  setImmediate(() => console.log("immediate 2 finished"));
+
+  process.nextTick(() => console.log("Process NextTick"));
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "PassWord encrypted");
+  });
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "PassWord encrypted");
+  });
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "PassWord encrypted");
+  });
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "PassWord encrypted");
+  });
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "PassWord encrypted");
+});
+
+console.log("Hello from the top-level code");
---
+++ 2-how-node-works/events.js
@@ -0,0 +1,47 @@
+const EventEmitter = require("events");
+const http = require("http");
+
+class sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+
+const myEmitter = new sales();
+
+myEmitter.on("newSale", () => {
+  console.log("There was a new sale");
+});
+
+myEmitter.on("newSale", () => {
+  console.log("Customer name: Wisdom");
+});
+
+myEmitter.on("newSale", (stock) => {
+  console.log(`There are now ${stock} item in the stock.`);
+});
+
+myEmitter.emit("newSale", 9);
+
+//////////////////////////////////////////////////////////
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("Request Received!");
+  console.log(req.url);
+  res.end("Request Received!");
+});
+
+server.on("request", (req, res) => {
+  console.log("Another Request 😃");
+});
+
+server.on("close", () => {
+  console.log("Server Closed");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Waiting for requests...");
+});
+
+// Error
