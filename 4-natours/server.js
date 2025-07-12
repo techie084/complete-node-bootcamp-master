f03c892840621c2f81a13d5aeafb04e428890469
Techie 🤸️🤍️ <wisdommatthew08@gmail.com>
2025-07-12 02:01:11 +0100

    changes

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,7 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    // useNewUrlParse: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successful!');
+  });
 
 // --------------- Server ---------------
 const port = process.env.PORT || 3000;
