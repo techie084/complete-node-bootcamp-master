ff7885453ddc1e2b94ff60ecfc0d8430e26960d4
Wisdom Matthew <wisdommatthew08@gmail.com>
2026-01-20 23:02:12 +0100

    changes

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -24,17 +24,11 @@ const getDogPic = async () => {
     const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
 
-    const res1Pro = superagent.get(
-      `https://dog.ceo/api/breed/${data}/images/random`,
-    );
+    const res1Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
 
-    const res2Pro = superagent.get(
-      `https://dog.ceo/api/breed/${data}/images/random`,
-    );
+    const res2Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
 
-    const res3Pro = superagent.get(
-      `https://dog.ceo/api/breed/${data}/images/random`,
-    );
+    const res3Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
 
     const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
     const imgs = all.map((el) => el.body.message);
--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,23 +1,26 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-// process.on('uncaughtException', (err) => {
-//   console.log('UNCAUGHT EXCEPTION! Shutting down...');
-//   console.log(err.name, err.message);
-//   process.exit(1);
-// });
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-// const DB = process.env.DATABASE.replace(
-//   'PASSWORD',
-//   process.env.DATABASE_PASSWORD,
-// );
+const DB = process.env.DATABASE.replace(
+  'PASSWORD',
+  process.env.DATABASE_PASSWORD,
+);
 
-mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+mongoose.connect(DB).then(() => {
   console.log('DB connection successful!');
 });
+// mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+//   console.log('DB connection successful!');
+// });
 
 // --------------- Server ---------------
 const port = process.env.PORT || 3000;
